d1df4c8627fd0922c657b5af5b0b5dea58c5fd6e
printyja <70492660+printyja@users.noreply.github.com>
2026-08-08 20:13:53 -0400

    updated logo and title in the header component, added favicon and updated title in index.html, improved error handling in VehicleForm.jsx, and fixed vehicle addition logic in App.jsx.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import { Truck } from "lucide-react";
-
+import logo from "../assets/logo.png";
 function Header() {
   return (
     <header className="header">
       <div className="header-inner">
-        <Truck size={28} color="white" />
-        <h1 className="header-title">My Fleet Asset Tracking </h1>
+        <img src={logo} alt="Logo" className="header-logo" />
+        <h1 className="header-title">My Fleet Management </h1>
       </div>
       {/* <p className="header-subtitle">Live Fleet Asset Tracking</p> */}
     </header>
